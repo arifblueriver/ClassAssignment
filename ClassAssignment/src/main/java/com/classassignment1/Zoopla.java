404bbc1f8bb8f1eeb39b2d948a04f0e440206b78
Arif <Arif@Arifur>
2019-12-13 01:02:06 -0500

    12/12/2019

--- ClassAssignment/src/main/java/com/classassignment1/Zoopla.java
+++ ClassAssignment/src/main/java/com/classassignment1/Zoopla.java
@@ -1,31 +1,41 @@
 package com.classassignment1;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import com.config.ObjectMap;
 import com.masterpagefactory.MasterPageFactory;
 import com.util.HighLighter;
 import com.util.TakeScreenShot;
 
+
+
+;
+
 public class Zoopla {
-	public static void main(String[] args) throws IOException {
+	
+	WebDriver driver;
+	@BeforeTest
+	public void setup(){
 		
 		
 	System.setProperty("webdriver.chrome.driver", 
 	"./Driver/chromedriver.exe");
 	
-	WebDriver driver=new ChromeDriver();
+	driver=new ChromeDriver();
 	driver.manage().deleteAllCookies();
 	driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	driver.manage().window().maximize();
-	
-	
+	}
+	@Test
+	public void login() throws Throwable{
 	ObjectMap obj= new ObjectMap();
 	driver.get(obj.getconfig("URL"));
 	
@@ -43,7 +53,10 @@ public class Zoopla {
 	pf.getPassword().sendKeys(obj.getconfig("Password"));
 	
 	pf.getPassword().submit();
+	}
 	
+	@AfterTest
+	public void teardown() {
 	TakeScreenShot.captureScreenShot(driver, "Zoopla login page");
 	driver.quit();
 	
